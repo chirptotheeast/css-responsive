9c67b9e7b772eef8c83cbe5671f8a6594773e22f
chirptotheeast <alyssa.easterly@yahoo.com>
2021-04-16 13:48:39 -0400

    updates

---
+++ test.js
@@ -0,0 +1,14 @@
+let hp = {"firstName":"Harry", "lastName":"Potter" }
+
+let myObj = JSON.parse(hp)
+
+
+
+//json object
+let obj = {"name":"harry"}
+
+
+
+//javascript object
+let obj = {name: 'harry'}
+
